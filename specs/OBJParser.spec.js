7055d881edcd9859e68f2bcf8c3e450eecfdc2ec
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-04-14 14:30:46 +0200

    feat(dual server/browser build): overall upgrade: - updated package, dependencies , build process - cleaned up main file & test - minor fix in main - added builds

--- specs/OBJParser.spec.js
+++ specs/OBJParser.spec.js
@@ -1,32 +1,28 @@
 import assert from 'assert'
 import fs from 'fs'
 
-//these two are needed by the parser
+// these two are needed by the parser
 import Rx from 'rx'
 import assign from 'fast.js/object/assign'
 
+import parse, { outputs } from '../src/index'
 
-import parse,  {outputs} from '../src/index'
+describe('OBJ parser tests', () => {
 
-describe("OBJ parser tests", () => {
-  
-  it("can parse obj files", function(done){
-    this.timeout(5000)
-    let data = fs.readFileSync("specs/data/box.obj",'binary')
-    let obs = parse(data) //we get an observable back
+  it('can parse obj files', function (done) {
+    this.timeout(10000)
+    let data = fs.readFileSync('specs/data/box.obj', 'binary')
+    let obs = parse(data) // we get an observable back
 
     obs
-      .filter( data => (!data.hasOwnProperty("progress")) ) //filter out progress information
-      .forEach(function(parsedGeometry){
+      // tap(e => console.log('data', e) )
+      .filter(data => (!data.hasOwnProperty('progress'))) // filter out progress information
+      .forEach(function (parsedGeometry) {
+        assert.equal(parsedGeometry.indices.length, 36)
+        assert.equal(parsedGeometry.positions.length, 108)
+        assert.equal(parsedGeometry.normals.length , 108)
 
-        assert.equal( parsedGeometry.indices.length, 36)
-        assert.equal( parsedGeometry.positions.length, 108)
-        assert.equal( parsedGeometry.normals.length , 108)
-        
         done()
-    })
+      })
   })
-  
 })
-
-
--- src/index.js
+++ src/index.js
@@ -9,62 +9,58 @@ import Rx from 'rx'
 
 import OBJ from './obj'
 
-import {createModelBuffers} from './parseHelpers'
+import { createModelBuffers } from './parseHelpers'
 
-export const outputs = ["geometry"] //to be able to auto determine data type(s) fetched by parser
-export const inputDataType = "arrayBuffer" //to be able to set required input data type 
+export const outputs = ['geometry'] // to be able to auto determine data type(s) fetched by parser
+export const inputDataType = 'arrayBuffer' // to be able to set required input data type
 
-export default function parse(data, parameters={}){
-
+export default function parse (data, parameters = {}) {
   const defaults = {
-    useWorker: (detectEnv.isBrowser===true)
-    ,offsets: [0]
+    useWorker: (detectEnv.isBrowser === true),
+    offsets: [0]
   }
-  parameters = assign({},defaults,parameters)
+  parameters = assign({}, defaults, parameters)
 
   const {useWorker, offsets} = parameters
   const obs = new Rx.ReplaySubject(1)
 
-  if ( useWorker ) {
-    let worker = new Worker( "./worker.js" )//browserify 
+  if (useWorker) {
+    let worker = new Worker('./worker.js') // browserify
 
-    worker.onmessage = function( event ) {
-      if("data" in event.data)
-      {
-        var data = event.data.data;
-        data.objects.forEach( (modelData,index) => {
-          obs.onNext({progress: (index+1)/Object.keys(data.objects).length, total:undefined})
-          obs.onNext( createModelBuffers( modelData ) )
-        })   
-        obs.onNext({progress: 1, total:undefined}) 
-        obs.onCompleted() 
+    worker.onmessage = function (event) {
+      if ('data' in event.data) {
+        var data = event.data.data
+        data.objects.forEach((modelData, index) => {
+          obs.onNext({progress: (index + 1) / Object.keys(data.objects).length, total: undefined})
+          obs.onNext(createModelBuffers(modelData))
+        })
+        obs.onNext({progress: 1, total: undefined})
+        obs.onCompleted()
       }
-      else if("progress" in event.data)
-      {
-        //console.log("got progress", event.data.progress);
-        obs.onNext({progress: event.data.progress, total:Math.NaN})
+      else if ('progress' in event.data) {
+        // console.log("got progress", event.data.progress)
+        obs.onNext({progress: event.data.progress, total: Math.NaN})
       }
-
     }
-    worker.onerror = function( event ){
+    worker.onerror = function (event) {
       obs.onError(`filename:${event.filename} lineno: ${event.lineno} error: ${event.message}`)
     }
-    worker.postMessage( {data} )
-    obs.catch(e=>worker.terminate()) 
-  } 
-  else 
-  {
-    data = new OBJ().getData( data )
+    worker.postMessage({data})
+    obs.catch(e => worker.terminate())
+  } else {
+    data = new OBJ().getData(data)
 
-    data.objects.forEach( (modelData,index) => {
-      obs.onNext({progress: (index+1)/Object.keys(data.objects).length, total:undefined})
-      obs.onNext( createModelBuffers( modelData ) )
-    })  
+    data.objects.forEach((modelData, index) => {
+      obs.onNext({progress: (index + 1) / Object.keys(data.objects).length, total: undefined})
+      obs.onNext(createModelBuffers(modelData))
 
-    obs.onNext({progress: 1, total:undefined}) 
-    obs.onCompleted()     
-    
+      if (index >= data.objects.length) { // FIXME: not too sure about this implementation
+        obs.onNext({progress: 1, total: undefined})
+        obs.onCompleted()
+      }
+    })
+
+
   }
   return obs
-
 }
